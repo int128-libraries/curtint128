9dc19252b694defed9b2338cb15d4236ad103326
Dmitry Mikushin <dmitry@kernelgen.org>
2020-10-23 01:41:03 +0200

    Setting up the CUDA test case with Thrust

---
+++ src/test128cuda.cu
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <stdint.h>
+#include <thrust/for_each.h>
+#include <thrust/iterator/counting_iterator.h>
+
+#include "cuda_uint128.h"
+
+int main(int argc, char ** argv)
+{
+  uint128_t x = (uint128_t) 1 << 120;
+
+  if (argc == 2)
+    x = string_to_u128((std::string)argv[1]);
+
+  thrust::counting_iterator<uint64_t> v(0);
+  thrust::for_each(v + 2, v + (1u << 30),
+    [x] __device__ (uint64_t v) {
+      uint64_t r;
+      uint128_t y = uint128_t::div128to128(x, v, &r);
+      uint128_t z = mul128(y, v) + r;
+
+      if (z != x)
+        printf("Error : y (%s) * v (%lu) + r (%lu) != x (%s)\n",
+          "u128_to_string(y).c_str()", v, r, "u128_to_string(x).c_str()");
+    });
+
+  printf("Done!\n");
+
+  return 0;
+}
