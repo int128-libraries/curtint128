0e81bdca56d76051e42ec092f64c643f48f79c96
Dmitry Mikushin <dmitry@kernelgen.org>
2020-10-22 23:55:23 +0200

    Configuring cpu test

--- include/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef CUDA_UNIT64_UTILS_H
-#define CUDA_UINT64_UTILS_H
-
-uint128_t calc(char * argv);
-
-#endif // CUDA_UINT64_UTILS_H
-
--- src/test128cpu.cpp
+++ src/test128cpu.cpp
@@ -1,6 +1,6 @@
+#include <cstdio>
 #include <stdint.h>
 #include <iostream>
-#include <omp.h>
 
 #include "cuda_uint128.h"
 
@@ -8,18 +8,21 @@ int main(int argc, char ** argv)
 {
   uint128_t x = (uint128_t) 1 << 120;
 
-  if(argc == 2)
+  if (argc == 2)
     x = string_to_u128((std::string)argv[1]);
 
   #pragma omp parallel for
-  for(uint64_t v = 2; v < 1u << 30; v++){
+  for (uint64_t v = 2; v < 1u << 30; v++) {
     uint64_t r;
     uint128_t y = uint128_t::div128to128(x, v, &r);
     uint128_t z = mul128(y, v) + r;
 
-    if(z != x) std::cout << z << std::endl;
+    if (z != x)
+      fprintf(stderr, "Error : y (%s) * v (%lu) + r (%lu) != x (%s)\n",
+        u128_to_string(y).c_str(), v, r, u128_to_string(x).c_str());
   }
 
-  return 0;
+  printf("Done!\n");
 
+  return 0;
 }
